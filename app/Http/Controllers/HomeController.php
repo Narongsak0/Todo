af6b9cdc5ac84f32ec098dba3a35687fdba571be
Narongsak Munmang <60143212@g.cmru.ac.th>
2020-07-18 16:30:50 +0700

    เพิ่มเติม

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(){
+        return view('home');
+
+    }
+
+    public function show(){
+    }
+
+    public function create(){
+    }
+
+    public function store(){
+    }
+
+    public function update(){
+    }
+
+    public function delete(){
+    }
+}
